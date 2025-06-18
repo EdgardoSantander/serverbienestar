d1b8a42abc6042ff873a028ab18679006f94de0d
EdgardoSantander <edgarsa1209@gmail.com>
2025-06-18 09:32:28 -0600

    creacion de proyecto

---
+++ src/main/java/mx/buap/bienestaremocional/BienestaremocionalApplication.java
@@ -0,0 +1,13 @@
+package mx.buap.bienestaremocional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BienestaremocionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BienestaremocionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/buap/bienestaremocional/BienestaremocionalApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.buap.bienestaremocional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BienestaremocionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
