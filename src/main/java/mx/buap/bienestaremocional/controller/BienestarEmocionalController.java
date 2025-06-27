bb711e57562ec44d05e830c5c06315a02a320a33
EdgardoSantander <edgarsa1209@gmail.com>
2025-06-27 14:18:42 -0600

    actualizacion de endpoints

--- src/main/java/mx/buap/bienestaremocional/controller/BienestarEmocionalController.java
+++ src/main/java/mx/buap/bienestaremocional/controller/BienestarEmocionalController.java
@@ -1,25 +1,37 @@
 package mx.buap.bienestaremocional.controller;
 
 import mx.buap.bienestaremocional.dto.MetricaDTO;
+import mx.buap.bienestaremocional.service.MetricaService;
+import mx.buap.bienestaremocional.service.MetricaServiceImple;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/bienestar-emocional")
 public class BienestarEmocionalController {
 
+    public MetricaService metricaService = new MetricaServiceImple();
 
     @GetMapping("/todos")
-    public ResponseEntity<?> getAllMetrics(){
+    public ResponseEntity<List<MetricaDTO>> getAllMetrics(){
 
-        int personaje = 5;
-        return ResponseEntity.ok(personaje);
+        return ResponseEntity.ok(metricaService.getAllMetricas());
+
+    }
+
+    @GetMapping("/buscar")
+    public ResponseEntity<?> getMetrica(@PathVariable Long id){
+
+        return ResponseEntity.ok(metricaService.getMetrica(id));
 
     }
 
     @PostMapping("/obtener")
     public ResponseEntity<?> setBodyMovile(@RequestBody MetricaDTO metricaDTO){
 
+        metricaService.guardarMetrica(metricaDTO);
 
         return ResponseEntity.ok(metricaDTO);
     }
